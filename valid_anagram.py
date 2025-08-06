14ae89872b9ff9e045b1ea30dfee41f22185fcf4
Arthur <arthurschiller10@gmail.com>
2025-08-06 16:10:56 -0300

    estrutura certa

--- valid_anagram.py
+++ valid_anagram.py
@@ -1,33 +1,40 @@
-def is_anagram(s, t):
-    if len(s) != len(t):
-        return False
-    
-    count_s = {}
-    for char in s:
-        if char in count_s:
-            count_s[char] += 1
-        else:
-            count_s[char] = 1
-    
-    print(count_s)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        count_s = {}
+        for char in s:
+            if char in count_s:
+                count_s[char] += 1
+            else:
+                count_s[char] = 1
+        
+        print(count_s)
 
-    for char in t:
-        if char in count_s:
-            count_s[char] -= 1
-        else:
-            return False
-    print(count_s)
-    for count in count_s.values():
-        if count != 0:
-            return False
-    
-    return True
+        for char in t:
+            if char in count_s:
+                count_s[char] -= 1
+            else:
+                return False
+        print(count_s)
+        for count in count_s.values():
+            if count != 0:
+                return False
+        
+        return True
+
+solution = Solution()
+
 
 # Teste
 s = 'arturz'
 t = 'arthru'
-print(is_anagram(s, t))  # False
-
+resultado1 = solution.isAnagram(s,t)
 s2 = 'listen'
 t2 = 'silent'
-print(is_anagram(s2, t2))  # True
+resultado2 = solution.isAnagram(s2,t2)
+
+print(resultado1)
+print(resultado2)
+
